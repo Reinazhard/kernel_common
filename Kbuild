GrapheneOS/kernel_google-modules_wlan_bcmdhd_bcm4398

dhd: Enable dhd to receive PCIE_CPL_TIMEOUT event
Change-Id: Id5b2d56b63ae6525bf283d54958beef94dad6d02

Kbuild
@@ -901,7 +901,7 @@ ifneq ($(CONFIG_SOC_GOOGLE),)
 	    DHDCFLAGS += -DBCMPCI_NOOTP_DEV_ID=0x4398 -DBCM4398_CHIP_DEF
     endif
     ifneq ($(CONFIG_BCMDHD_PCIE),)
-    ifneq ($(CONFIG_SOC_GS201),)
+    ifneq ($(filter y, $(CONFIG_SOC_GS201) $(CONFIG_SOC_ZUMA)),)
 	DHDCFLAGS += -DPCIE_CPL_TIMEOUT_RECOVERY
     endif
     endif

Reinazhard/kernel_common

Kbuild
@@ -935,7 +935,7 @@ ifneq ($(CONFIG_BCMDHD_PCIE),)
 	DHDCFLAGS += -DDHD_NVRAM_NAME="\"bcmdhd.cal\""
 	DHDCFLAGS += -DDHD_CLM_NAME="\"bcmdhd_clm.blob\""
 	DHDCFLAGS += -DDHD_MAP_NAME="\"fw_bcmdhd.map\""
-ifneq ($(CONFIG_SOC_GS201),)
+ifneq ($(filter y, $(CONFIG_SOC_GS101) $(CONFIG_SOC_GS201)),)
 	DHDCFLAGS += -DCPL_TIMEOUT_RECOVERY
 endif
 endif