7d891a21bee6f5ad14ec3f04978a25500caa1a59
Dyneteve <dyneteve@hentaios.com>
2024-04-04 20:54:53 +0000

    Revert "max77759_charger: Add UVLO1 identifcation"
    This reverts commit 29e8ad6821ce4c1ade981f3f79ffd5a1f2c518e8.

--- max77759_charger.c
+++ max77759_charger.c
@@ -363,12 +363,8 @@ static int max77759_get_and_clr_bcl_irq(struct i2c_client *client, u8 *irq_val)
 
 	if (chg_int & MAX77759_CHG_INT2_SYS_UVLO2_I)
 		*irq_val = UVLO2;
-	else if (chg_int & MAX77759_CHG_INT2_BAT_OILO_I)
-		*irq_val = BATOILO;
-	else if (chg_int & MAX77759_CHG_INT2_SYS_UVLO1_I)
-		*irq_val = UVLO1;
 	else
-		*irq_val = 0;
+		*irq_val = BATOILO;
 
 	ret = max77759_reg_write(data->regmap, MAX77759_CHG_INT2, chg_int);
 	if (ret < 0)
