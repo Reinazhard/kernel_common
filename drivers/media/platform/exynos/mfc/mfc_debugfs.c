dbc50a8a049367537ba8d82dd1cd32fd8245f414
Dyneteve <dyneteve@hentaios.com>
2024-04-25 11:40:30 +0000

    Revert "media: mfc: Disable idle suspend support on older platforms"
    We bumped the MFC FW.
    
    This reverts commit fd9805ac2617b9c6623574ef73bf9f786c5db95a.
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- drivers/media/platform/exynos/mfc/mfc_debugfs.c
+++ drivers/media/platform/exynos/mfc/mfc_debugfs.c
@@ -45,11 +45,7 @@ unsigned int core_balance;
 unsigned int sbwc_disable;
 unsigned int sscd_report;
 unsigned int hdr_dump;
-#if IS_ENABLED(CONFIG_SOC_ZUMA)
 unsigned int idle_suspend_enable = 1;
-#else
-unsigned int idle_suspend_enable = 0;
-#endif
 
 static int __mfc_info_show(struct seq_file *s, void *unused)
 {
