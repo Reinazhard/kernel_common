571cb4f430fbac8304dcc4cc093b90f09f84d0c4
Reinazhard <reinazhard@gmail.com>
2024-08-08 17:35:09 +0700

    cpuiflde aodjoas
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- google-modules/soc/gs/include/soc/google/cpuidle_metrics.h
+++ google-modules/soc/gs/include/soc/google/cpuidle_metrics.h
@@ -11,7 +11,7 @@ int cpuidle_metrics_init(struct kobject *metrics_kobj);
 void cpuidle_metrics_histogram_register(char* name, int cluster_id, s64 target_residency_us);
 #else
 static inline void cpuidle_metrics_histogram_append(int cluster_id, s64 time_us) {};
-static int cpuidle_metrics_init(struct kobject *metrics_kobj) { return 0; };
+static int __maybe_unused cpuidle_metrics_init(struct kobject *metrics_kobj) { return 0; };
 static void cpuidle_metrics_histogram_register(char* name, int cluster_id,
                                 s64 target_residency_us) {};
 #endif
